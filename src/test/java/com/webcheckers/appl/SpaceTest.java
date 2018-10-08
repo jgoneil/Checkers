3996a99dd1d868ba5511fe74773aadc0a5bec1cc
pjs6747 <pjs6747@rit.edu>
2018-10-08 16:02:48 -0400

    Style Fix
    Was using 4 spaces instead of 2

--- src/test/java/com/webcheckers/appl/SpaceTest.java
+++ src/test/java/com/webcheckers/appl/SpaceTest.java
@@ -7,46 +7,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SpaceTest {
 
-    private Space space1;
-    private Space space2;
-    
-    @BeforeEach
-    void setUp() {
-        space1 = new Space(1,2, Space.Color.BLACK);
+  private Space space1;
+  private Space space2;
 
-    }
+  @BeforeEach
+  void setUp() {
+    space1 = new Space(1,2, Space.Color.BLACK);
+  }
 
-    @Test
-    void getColor() {
-        space2 = new Space(3,2, Space.Color.WHITE);
-        assert (space1.getColor() == Space.Color.BLACK);
-        assert (space2.getColor() == Space.Color.WHITE);
-    }
+  @Test
+  void getColor() {
+    space2 = new Space(3,2, Space.Color.WHITE);
+    assert (space1.getColor() == Space.Color.BLACK);
+    assert (space2.getColor() == Space.Color.WHITE);
+  }
 
-    @Test
-    void occupy() {
-        space1.occupy();
-        assert (space1.isOccupied());
-    }
-    
-    @Test
-    void isOccupied() {
-        
-    }
-    
-    @Test
-    void unoccupy() {
-        space1.occupy();
-        assert (space1.isOccupied());
-        space1.unopccupy();
-        assert (!space1.isOccupied());
-    }
+  @Test
+  void occupy() {
+    space1.occupy();
+    assert (space1.isOccupied());
+  }
 
-    @Test
-    void cellIdx() {
-        space2 = new Space(1,2, Space.Color.WHITE);
-        assert (space1.cellIdx().equals(space2.cellIdx()));
-    }
+  @Test
+  void isOccupied() {
+
+  }
+
+  @Test
+  void unoccupy() {
+    space1.occupy();
+    assert (space1.isOccupied());
+    space1.unopccupy();
+    assert (!space1.isOccupied());
+  }
+
+  @Test
+  void cellIdx() {
+    space2 = new Space(1,2, Space.Color.WHITE);
+    assert (space1.cellIdx().equals(space2.cellIdx()));
+  }
 
 
 }
