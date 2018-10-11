73d0a7a7805a4417fb6ec02bd52986c8d0e98bf5
pedro <pab3507@rit.edu>
2018-10-11 10:18:44 -0400

    Complete Row testing

---
+++ src/test/java/com/webcheckers/appl/PieceTest.java
@@ -0,0 +1,38 @@
+package com.webcheckers.appl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PieceTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void move() {
+  }
+
+  @Test
+  void getSpace() {
+  }
+
+  @Test
+  void type() {
+  }
+
+  @Test
+  void color() {
+  }
+
+  @Test
+  void isValidMove() {
+  }
+}
--- src/test/java/com/webcheckers/appl/RowTest.java
+++ src/test/java/com/webcheckers/appl/RowTest.java
@@ -2,19 +2,35 @@ package com.webcheckers.appl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RowTest {
+  Row row;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    row = new Row(1, 4, "red");
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    row = null;
+  }
 
   @Test
   void getRow() {
+    assertNotNull(row.getRow());
   }
 
   @Test
   void index() {
+    assertEquals(1, row.index());
   }
 
   @Test
   void iterator() {
+    assertNotNull(row.iterator());
   }
 }
