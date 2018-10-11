2e28b9a1ee40529a22c74bd033052badc713b760
pedro <pab3507@rit.edu>
2018-10-11 09:42:06 -0400

    Complete Board testing

--- src/test/java/com/webcheckers/appl/BoardTest.java
+++ src/test/java/com/webcheckers/appl/BoardTest.java
@@ -1,10 +1,8 @@
 package com.webcheckers.appl;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
@@ -14,11 +12,18 @@ public class BoardTest {
   private Player redPLayer;
   private Board board1;
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
     this.whitePlayer = new Player("White");
     this.redPLayer = new Player("Red");
     this.board1 = new Board(redPLayer, whitePlayer, 8, "red");
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    whitePlayer = null;
+    redPLayer = null;
+    board1 = null;
   }
 
   @Test
---
+++ src/test/java/com/webcheckers/appl/RowTest.java
@@ -0,0 +1,20 @@
+package com.webcheckers.appl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RowTest {
+
+  @Test
+  void getRow() {
+  }
+
+  @Test
+  void index() {
+  }
+
+  @Test
+  void iterator() {
+  }
+}
