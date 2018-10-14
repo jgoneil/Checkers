64ba518093f6b3e8c1e6c75d829e8af0a9a69b58
pedro <pab3507@rit.edu>
2018-10-14 11:57:21 -0400

    Complete testing CheckMove class

--- src/test/java/com/webcheckers/model/CheckMoveTest.java
+++ src/test/java/com/webcheckers/model/CheckMoveTest.java
@@ -3,28 +3,38 @@ package com.webcheckers.model;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
+import com.webcheckers.appl.Player;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CheckMoveTest {
 
-  private ModelBoard mockModelBoard;
+  private ModelBoard modelBoard;
   private CheckMove checkMove;
-
+  private Player redPlayerMock;
+  private Player whitePlayerMock;
+  private Position whiteGoal;
   @BeforeEach
   void setUp() {
-    mockModelBoard = mock(ModelBoard.class);
-    checkMove = new CheckMove(mockModelBoard);
+    redPlayerMock = mock(Player.class);
+    whitePlayerMock = mock(Player.class);
+
+    modelBoard = new ModelBoard(redPlayerMock, whitePlayerMock, 8);
+    checkMove = new CheckMove(modelBoard);
+
+    whiteGoal = new Position(0,0);
   }
 
   @AfterEach
   void tearDown() {
-    mockModelBoard = null;
+    modelBoard = null;
+    checkMove = null;
   }
 
   @Test
   void validateMove() {
-//    assertEquals(checkMove.validateMove(new Position(), new Position()));
+    assertTrue(checkMove.validateMove(new Position(1,0), whiteGoal).containsKey(false));
+    assertTrue(checkMove.validateMove(new Position(0,0), new Position(0,1)).containsKey(false));
   }
 }
