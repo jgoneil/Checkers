a616f4c13474e87bdc884b9416531c9f33cfe7ab
pedro <pab3507@rit.edu>
2018-10-18 01:40:38 -0400

    Test new checks in CheckMove

--- src/test/java/com/webcheckers/model/CheckMoveTest.java
+++ src/test/java/com/webcheckers/model/CheckMoveTest.java
@@ -8,25 +8,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @Tag("Model-tier")
 class CheckMoveTest {
 
   private ModelBoard modelBoard;
   private CheckMove checkMove;
-  private Player redPlayerMock;
-  private Player whitePlayerMock;
-  private Position whiteGoal;
+
+  private Position position54;
+  private Position position43;
+  private Position position34;
+  private Position position47;
+  private Position position63;
+  private Position position23;
+
+  // White square
+  private Position position44;
 
   @BeforeEach
   void setUp() {
-    redPlayerMock = mock(Player.class);
-    whitePlayerMock = mock(Player.class);
+    Player redPlayerMock = mock(Player.class);
+    Player whitePlayerMock = mock(Player.class);
 
     modelBoard = new ModelBoard(redPlayerMock, whitePlayerMock, 8);
     checkMove = new CheckMove(modelBoard);
 
-    whiteGoal = new Position(0, 0);
+    position54 = new Position(5, 4);
+    position43 = new Position(4, 3);
+    position34 = new Position(3, 4);
+    position47 = new Position(4, 7);
+    position63 = new Position(6, 3);
+    position23 = new Position(2, 3);
+    position44 = new Position(4, 4);
   }
 
   @AfterEach
@@ -35,11 +50,26 @@ class CheckMoveTest {
     checkMove = null;
   }
 
+
   @Test
-  void validateMove() {
-    assertTrue(checkMove.validateMove(new Position(1, 0), whiteGoal).containsKey(false));
-    assertTrue(checkMove.validateMove(new Position(0, 0), new Position(0, 1)).containsKey(false));
-    assertTrue(checkMove.validateMove(new Position(5, 2), new Position(4, 3)).containsKey(false));
-    assertTrue(checkMove.validateMove(new Position(5, 2), new Position(3, 0)).containsKey(false));
+  void validateMoveTest_ValidMove() {
+    assertTrue(checkMove.validateMove(position54, position43).containsKey(true));
+  }
+
+  @Test
+  void validateMoveTest_InvalidMove() {
+    // more than one space
+    assertTrue(checkMove.validateMove(position54, position34).containsKey(false));
+    assertTrue(checkMove.validateMove(position54, position47).containsKey(false));
+
+    // move backward
+    assertTrue(checkMove.validateMove(position54, position63).containsKey(false));
+
+    // move to white
+    assertTrue(checkMove.validateMove(position54, position44).containsKey(false));
+
+    // move to occupied
+    assertTrue(checkMove.validateMove(position34, position23).containsKey(false));
+
   }
 }
