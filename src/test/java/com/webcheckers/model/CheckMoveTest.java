a92d1b1b45d67e84be155843896efd470df908d2
pedro <pab3507@rit.edu>
2018-10-18 01:44:09 -0400

    remove irrelevant imports

--- src/test/java/com/webcheckers/model/CheckMoveTest.java
+++ src/test/java/com/webcheckers/model/CheckMoveTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 @Tag("Model-tier")
 class CheckMoveTest {
