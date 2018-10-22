512c8c84bbac22dc58ca187a7768c68d3029a50c
Jake <jgo8407@rit.edu>
2018-10-22 10:55:12 -0400

    Comment makeKing class

--- src/main/java/com/webcheckers/model/makeKing.java
+++ src/main/java/com/webcheckers/model/makeKing.java
@@ -1,8 +1,15 @@
 package com.webcheckers.model;
-
 import com.webcheckers.appl.Piece;
 
+/**
+ * Class to Convert a Single to a King
+ */
 public class makeKing {
+    /**
+     * Checks if a piece has made it to the other end of the Board so that it could become
+     * King or not.
+     * @param piece - Piece that is possibly being Kinged
+     */
   public void kingPiece(Piece piece){
     if(piece.getColor() == Piece.Color.RED){
       if(piece.getSpace().getCellIdx() == 0){
