2216c7f2c66b9343f23baf4483b6a08852fd4659
Jake <jgo8407@rit.edu>
2018-10-23 10:26:44 -0400

    Undid previous change after clarification

--- src/main/java/com/webcheckers/model/makeKing.java
+++ src/main/java/com/webcheckers/model/makeKing.java
@@ -1,6 +1,5 @@
 package com.webcheckers.model;
 import com.webcheckers.appl.Piece;
-import com.webcheckers.appl.Player;
 
 /**
  * Class to Convert a Single to a King
@@ -10,21 +9,16 @@ public class makeKing {
      * Checks if a piece has made it to the other end of the Board so that it could become
      * King or not.
      * @param piece - Piece that is possibly being Kinged
-     * @param player - Player controlling the piece that is being Kinged
      */
-  public void kingPiece(Piece piece, Player player) {
-    if (!player.getHasMoved()) {
-      if (piece.getColor() == Piece.Color.RED) {
-        if (piece.getSpace().getCellIdx() == 0) {
-          piece.King();
-          player.setHasMoved(true);
-        }
+  public void kingPiece(Piece piece) {
+    if (piece.getColor() == Piece.Color.RED) {
+      if (piece.getSpace().getCellIdx() == 0) {
+        piece.King();
       }
-      else if (piece.getColor() == Piece.Color.WHITE) {
-        if (piece.getSpace().getCellIdx() == 7) {
-          piece.King();
-          player.setHasMoved(true);
-        }
+    }
+    else if (piece.getColor() == Piece.Color.WHITE) {
+      if (piece.getSpace().getCellIdx() == 7) {
+        piece.King();
       }
     }
   }
