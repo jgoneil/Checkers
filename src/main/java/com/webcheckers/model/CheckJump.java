50367a68ec80eb06873c4787e01c7b28699a7f5a
pjs6747 <pjs6747@rit.edu>
2018-10-26 12:38:41 -0400

    Fixed Format to match others

--- src/main/java/com/webcheckers/model/CheckJump.java
+++ src/main/java/com/webcheckers/model/CheckJump.java
@@ -9,61 +9,61 @@ import java.util.Map;
 
 public class CheckJump {
 
-    private ModelBoard board;
+  private ModelBoard board;
 
-    public CheckJump(ModelBoard board){this.board = board;}
+  public CheckJump(ModelBoard board){this.board = board;}
 
-    private boolean isMovingDiagonal(Position start, Position end) {
-        int xDiff = Math.abs(start.getRow() - end.getRow());
-        int yDiff = Math.abs(start.getCell() - end.getCell());
-        return xDiff == yDiff;
+  private boolean isMovingDiagonal(Position start, Position end) {
+    int xDiff = Math.abs(start.getRow() - end.getRow());
+    int yDiff = Math.abs(start.getCell() - end.getCell());
+    return xDiff == yDiff;
+  }
+
+  private boolean isMovingTwo(Position start, Position end) {
+    int xDiff = Math.abs(start.getRow() - end.getRow());
+    int yDiff = Math.abs(start.getCell() - end.getCell());
+    return xDiff == 2 && yDiff == 2;
+  }
+
+  private boolean isMovingForward(Position start, Position end) {
+    return end.getRow() < start.getRow();
+  }
+
+  private boolean isJumpingPiece(Position start, Position end, Player player) {
+    Space middleSpace = board.getSpace(start.getRow() - end.getRow() - 1,
+            start.getCell() - end.getCell() - 1);
+    Piece middlePiece = middleSpace.getPiece();
+    return middleSpace.isOccupied() && middlePiece.getColor().toString().equals(player.getColor());
+  }
+
+  public Map<Boolean, String> validateJump(Position start, Position target, Player player) {
+    Map<Boolean, String> response = new HashMap<>();
+    Space current;
+    Space goal;
+    if(player.getColor().equals("Red")) {
+      current = board.getSpace(start.getRow(), start.getCell());
+      goal = board.getSpace(target.getRow(), target.getCell());
+    } else {
+      current = board.getSpace(7 - start.getRow(), 7 - start.getCell());
+      goal = board.getSpace(7 - target.getRow(), 7 - target.getCell());
     }
-
-    private boolean isMovingTwo(Position start, Position end) {
-        int xDiff = Math.abs(start.getRow() - end.getRow());
-        int yDiff = Math.abs(start.getCell() - end.getCell());
-        return xDiff == 2 && yDiff == 2;
+    if (goal.getColor().equals(Space.Color.WHITE)) {
+      response.put(false, "Attempted to move a piece to a white space.");
+    } else if (goal.isOccupied()) {
+      response.put(false, "Attempted to move a piece to an already occupied space");
+    } else if (!isMovingTwo(start, target)) {
+      response.put(false, "Attempted to move piece too far.");
+    } else if (!isMovingDiagonal(start, target)) {
+      response.put(false, "Pieces can only move diagonally.");
+    } else if (!isMovingForward(start, target)) {
+      response.put(false, "Piece can only move forward");
+    } else if (!isJumpingPiece(start, target, player)) {
+      response.put(false, "Must be jumping other players piece");
+    } else {
+      board.addPieceToSpace(current.getPiece(), goal);
+      current.unoccupy();
+      response.put(true, "This move is valid.");
     }
-
-    private boolean isMovingForward(Position start, Position end) {
-        return end.getRow() < start.getRow();
-    }
-
-    private boolean isJumpingPiece(Position start, Position end, Player player) {
-        Space middleSpace = board.getSpace(start.getRow() - end.getRow() - 1,
-                start.getCell() - end.getCell() - 1);
-        Piece middlePiece = middleSpace.getPiece();
-        return middleSpace.isOccupied() && middlePiece.getColor().toString().equals(player.getColor());
-    }
-
-    public Map<Boolean, String> validateJump(Position start, Position target, Player player) {
-        Map<Boolean, String> response = new HashMap<>();
-        Space current;
-        Space goal;
-        if(player.getColor().equals("Red")) {
-            current = board.getSpace(start.getRow(), start.getCell());
-            goal = board.getSpace(target.getRow(), target.getCell());
-        } else {
-            current = board.getSpace(7 - start.getRow(), 7 - start.getCell());
-            goal = board.getSpace(7 - target.getRow(), 7 - target.getCell());
-        }
-        if (goal.getColor().equals(Space.Color.WHITE)) {
-            response.put(false, "Attempted to move a piece to a white space.");
-        } else if (goal.isOccupied()) {
-            response.put(false, "Attempted to move a piece to an already occupied space");
-        } else if (!isMovingTwo(start, target)) {
-            response.put(false, "Attempted to move piece too far.");
-        } else if (!isMovingDiagonal(start, target)) {
-            response.put(false, "Pieces can only move diagonally.");
-        } else if (!isMovingForward(start, target)) {
-            response.put(false, "Piece can only move forward");
-        } else if (!isJumpingPiece(start, target, player)) {
-            response.put(false, "Must be jumping other players piece");
-        } else {
-            board.addPieceToSpace(current.getPiece(), goal);
-            current.unoccupy();
-            response.put(true, "This move is valid.");
-        }
-        return response;
-    }
+    return response;
+  }
 }
